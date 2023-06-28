0c9cdd3fc54b95173563cbb416c927ba8892abd1
irina_pr2020@mail.ru <irinakuznecova@MacBook-Air-Irina.local>
2023-06-28 15:50:22 +0300

    решение 6й задачи

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+Console.WriteLine( "Число " + number + " является ЧЕТНЫМ " );
+}
+else 
+{
+    Console.WriteLine( "Число " + number + " является  НЕЧЕТНЫМ " );
+}
