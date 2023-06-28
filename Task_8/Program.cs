4bb52eb30604f563092100a24dcac00aadd6391c
irina_pr2020@mail.ru <irinakuznecova@MacBook-Air-Irina.local>
2023-06-28 16:57:17 +0300

    решение задачи 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int countN = 1;
+// if {
+//    (N % 2 == 0);
+// }
+while (countN <= N)
+ {
+         Console.Write(countN + " ");
+        countN ++;
+ }
