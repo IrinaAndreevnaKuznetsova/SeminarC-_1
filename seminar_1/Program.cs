c511e6c4976ee75746946c845811ef7c602a8d45
irina_pr2020@mail.ru <irinakuznecova@MacBook-Air-Irina.local>
2023-06-28 15:21:23 +0300

    задача 2 решена

---
+++ seminar_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+int max = firstDigit;
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit  +  " больше чем " + secondDigit);
+}
+
+else 
+{
+Console.WriteLine (firstDigit  + " меньше чем " + secondDigit);
+}
+
