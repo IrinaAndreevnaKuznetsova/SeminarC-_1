6f3a832ecde20ca38c47c265ebf4eb1d87096a2a
irina_pr2020@mail.ru <irinakuznecova@MacBook-Air-Irina.local>
2023-06-29 16:46:45 +0300

    fixed a bug

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,11 +1,17 @@
-﻿Console.Write("Введите число: ");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int countN = 1;
-// if {
-//    (N % 2 == 0);
-// }
 while (countN <= N)
+{
+        if(countN % 2 == 0)
+
  {
          Console.Write(countN + " ");
+ }
         countN ++;
  }
